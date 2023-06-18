bcccfc0023e49af62a6bc39a33526f954161493c
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-06-18 16:54:24 +0530

    updated some styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,11 +18,9 @@ const Header = () => {
       <h1 className={classes.h}>MY GENERICS</h1>
 <Link to="/Home" > <button className={classes.buton}>HOME</button>  </Link>
 <Link to="/ContactUs" >   <button  className={classes.buton}>CONTACT</button></Link>
+<Link to="/AboutUs" >  <button className={classes.buton}>ABOUT</button> </Link>
  <Link to="/products" >  <button className={classes.buton}>STORE</button> </Link>  
      
-    <Link to="/AboutUs" >  <button className={classes.buton}>ABOUT</button> </Link>
-       
-
       <Link to="/Login" ><button className={classes.login}>LOGIN</button></Link>
       {authctx.token && <CartButton></CartButton>}
      {authctx.token&&<Link  to="/Login" onClick={ authctx.logout} ><button className={classes.logout}>LOGOUT</button> </Link>}
